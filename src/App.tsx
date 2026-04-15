0c7ae9554cdc298e3ebda030227fddeac27b2118
APNR03 <154341256+APNR03@users.noreply.github.com>
2026-04-15 12:23:13 -0500

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,5 @@
 import { Switch, Route, Router as WouterRouter } from "wouter";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { Toaster } from "@/components/ui/toaster";
-import { TooltipProvider } from "@/components/ui/tooltip";
 import { useEffect } from "react";
 import { MapPin, Phone } from "lucide-react";
 
@@ -145,14 +143,11 @@ function Router() {
 
 function App() {
   return (
-    <QueryClientProvider client={queryClient}>
-      <TooltipProvider>
-        <WouterRouter base={import.meta.env.BASE_URL.replace(/\/$/, "")}>
-          <Router />
-        </WouterRouter>
-        <Toaster />
-      </TooltipProvider>
-    </QueryClientProvider>
+   <QueryClientProvider client={queryClient}>
+  <WouterRouter>
+    <Router />
+  </WouterRouter>
+</QueryClientProvider>
   );
 }
 
