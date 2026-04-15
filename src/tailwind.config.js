bb4f06c1878be3699bf807558ffa91a0c0c1f57f
APNR03 <154341256+APNR03@users.noreply.github.com>
2026-04-15 13:06:23 -0500

    Update tailwind.config.js

--- src/tailwind.config.js
+++ src/tailwind.config.js
@@ -4,6 +4,13 @@ export default {
     "./index.html",
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
+  safelist: [
+    "bg-background",
+    "text-foreground",
+    "bg-card",
+    "text-primary",
+    "border-white/5",
+  ],
   theme: {
     extend: {},
   },
